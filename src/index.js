f1a17b26aa21afb7a6b3c634e328512f9e90213d
e <751232478@qq.com>
2020-03-01 14:03:34 +0800

    demo3月1日的初始化

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+//-----------2-29------------------------
+// const time = '2月29日'  
+// const div = (<div className='box'><h2>今天是:{time} JSX初体验</h2></div>)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//-----------3-01------------------------
+// const isloaded =false
+// if else 下的
+
+// const loading = ()=> {
+//   if(isloaded){
+//     return <div>加载完毕！！！</div>
+//   }else {
+//     return <div>加载中。。。</div>
+//   }
+// }
+
+//三元表达式
+
+// const loadinga = isloaded? <div>加载完毕！！！</div> : <div>加载中。。。</div>
+
+//循环输出数组项
+const subjects = [
+                  {id: 1, name: '高数'},   
+                  {id: 2, name: '毛概'}, 
+                  {id: 3, name: '马原'}, 
+                  {id: 4, name: '近代史'}, 
+                  {id: 5, name: '思修'}
+                  ]
+const list = (   <ul className = "ullist"> {subjects.map(item => <li>{item.name}</li>)}   </ul> ) 
+
+// const div = (<div className='box'>{loading()}</div>)
+// const div = (<div className='box'>{loadinga}</div>)
+const div = (<div className='box'><h2>必修公共基础课</h2>{list}</div>)
+ReactDOM.render(div, document.getElementById('root'));
